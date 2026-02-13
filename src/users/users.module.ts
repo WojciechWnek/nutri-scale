5b2e100f18d04045cbe1c07630a30db47af0d0ee
WojciechWnek <wownek@gmail.com>
2026-02-12 21:04:10 -0600

    remove guard and strategy

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, JwtStrategy],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
