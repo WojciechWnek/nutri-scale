499ca57c53a618da209522bf12401fbf1d2f51ea
WojciechWnek <wownek@gmail.com>
2026-03-08 21:33:33 -0500

    Update CORS policy

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,11 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
